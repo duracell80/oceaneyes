ffedde6b50a2ed38f79793259e74a77b948d2494
Lee Jordan <duracell80@users.noreply.github.com>
2023-07-28 21:44:49 -0500

    Improve Venv run script

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 DIR_PWD=$(pwd)
 DIR_APP=$DIR_PWD/app
+APP_NME=${PWD##*/}
+PIP_INS=$(which python | sed -n "s/.*\(${APP_NME}\).*/\1/p" | wc -l)
 
-if [ -d "./bin" ];then
-  echo "[i] Press Ctrl+D to deactivate"
-  bash -c ". bin/activate; exec /usr/bin/env bash --rcfile <(echo 'PS1=\"(oceaneyes)\${PS1}\"') -i"
-  cd ./app
+if [[ $PIP_INS == 0 ]]; then
+  echo -e "[i] Venv active for ${APP_NME} \n--- Type exit to deactivate \n--- Type ./run.sh again to run the app \n\n" && bash -c "source bin/activate; exec /usr/bin/env bash --rcfile <(echo 'PS1=\"(${APP_NME})\${PS1}\"') -i"
 else
-  echo "[i] Venv active ... cd ./app && ./main.py"
+  echo "[i] Running App from within ... $(pwd)"
+  cd $DIR_APP && $DIR_APP/main.py
 fi
