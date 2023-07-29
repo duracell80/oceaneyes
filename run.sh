43c26b980b3c13f59312b073801f02a85bf0f0c6
Lee Jordan <duracell80@users.noreply.github.com>
2023-07-28 22:01:10 -0500

    Venv autorun

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ APP_NME=${PWD##*/}
 PIP_INS=$(which python | sed -n "s/.*\(${APP_NME}\).*/\1/p" | wc -l)
 
 if [[ $PIP_INS == 0 ]]; then
-  echo -e "[i] Venv active for ${APP_NME} \n--- Type exit to deactivate \n--- Type ./run.sh again to run the app \n\n" && bash -c "source bin/activate; exec /usr/bin/env bash --rcfile <(echo 'PS1=\"(${APP_NME})\${PS1}\"') -i"
+  echo -e "[i] Venv active for ${APP_NME}, type exit to deactivate and ./run.sh to run the app in the venv"
+  bash -c "source bin/activate; exec /usr/bin/env bash --rcfile <(echo 'PS1=\"(${APP_NME})\${PS1}\"') -i"
 else
   echo "[i] Running App from within ... $(pwd)"
   cd $DIR_APP && $DIR_APP/main.py
