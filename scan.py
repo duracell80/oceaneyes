31126ba8896bf3dae2658b86a33fe0c6678c907c
Lee Jordan <duracell80@users.noreply.github.com>
2024-03-22 21:43:06 -0500

    Implement standalone device scanning

--- scan.py
+++ scan.py
@@ -1,33 +1,96 @@
-#!..bin/python3
-import os, socket, sys, subprocess, requests
+#!../bin/python3
+import sys, os, subprocess, time, logging, socket, requests, urllib, urllib.parse, json
+from tinydb import TinyDB, Query
 
-sys.exit()
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
 
 # Perform LAN scan
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("1.1.1.1", 80))
-h = s.getsockname()[0].split(".")
-h = str(h[0] + "." + h[1] + "." + h[2])
-s.close()
-
-hosts   = []
-devices = []
-
-print("[i] LAN scan in progress ... takes about 5 minutes")
-for i in range(1,256):
-	host 	= h + "." + str(i)
-	result 	= str(os.popen("ping " + str(host) + " -w 1").read())
-	if "1 received" in result:
-		r = requests.get("http://" + str(host) + "/php/favList.php?PG=0")
-		if r.status_code == 200:
-			devices.append(host)
-		hosts.append(host)
-
-radios = len(devices)
-if radios > 0:
-	if radios > 1:
-		print("\n[i] Found " + str(radios) + " Skytune radio on the network")
+def scan():
+	if os.path.isfile("settings.db"):
+		dbc     = TinyDB("settings.db")
 	else:
-		print("\n[i] Found " + str(radios) + " Skytune radios on the network")
-	for i in range(0,int(radios)):
-		print("--- Radio " + str(i+1) + " @" + str(devices[i]))
+		dbc     = TinyDB("settings.db")
+		dbc.insert({'ipaddress': '0.0.0.0', 'language': 'English'})
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("1.1.1.1", 80))
+	h = s.getsockname()[0].split(".")
+	h = str(h[0] + "." + h[1] + "." + h[2])
+	s.close()
+
+	hosts   = []
+	devices = []
+	d       = 0
+	e	= 99
+	logging.info("[i] Auto detecting Skytune radios on the LAN, this may take some time ...")
+	print("[i] Auto detecting Skytune radios on the LAN ... \n\nPress Ctrl + Z when all known devices are found")
+	print("Press Ctrl + C if scanning seems to be stuck\n\n")
+	print("--- Tip: Take note of these numbers and then set")
+	print("--- static routes on the router for these devices\n\n")
+	for i in range(1,256):
+		host    = h + "." + str(i)
+		try:
+			try:
+				r = requests.get("http://" + str(host) + ":80", timeout=1)
+				if r.status_code == 200:
+					p = 80
+			except:
+				p = 0
+				logging.info(f"[i] Looking for a radio at this address ({str(h)}.{str(i)})")
+
+			if p == 80:
+				logging.info(f"[i] Looking for a radio at this address ({str(h)}.{str(i)}) [P:80]")
+
+				# test that the radio can be pinged
+				result  = str(os.popen("ping " + str(host) + " -w 5 -W 5").read())
+			else:
+				p = 0
+				result = "0 received"
+		except:
+			p = 0
+			result  = "0 received"
+
+
+		if "5 received" in result:
+			try:
+				# Detect Skytune Radio
+				r = requests.get("http://" + str(host) + "/php/favList.php?PG=0")
+				if r.status_code == 200:
+					d+=1
+					devices.append(host)
+					dbc.update({'ipaddress': str(host)}, doc_ids=[int(d)])
+					logging.info(f"\n[+] Found a skytune radio @{str(host)}:80\n")
+
+			except:
+				x=1
+
+			try:
+				# Detect HDHomeRun
+				g = requests.get("http://" + str(host) + "/lineup.html")
+				if g.status_code == 200:
+					if "<title>HDHomeRun Lineup</title>" in g.text:
+						e+=1
+						devices.append(host)
+						dbc.update({'ipaddress': str(host)}, doc_ids=[int(e)])
+
+						logging.info(f"\n[+] Found a hdhomerun @{str(host)}:80\n")
+			except:
+				x=1
+
+			hosts.append(host)
+
+
+	radios = len(devices)
+	if radios > 0:
+		if radios > 1:
+			logging.info(f"\n[i] Found {str(radios)} tuners on the network")
+		else:
+			logging.info(f"\n[i] Found {str(radios)} tuner on the network")
+
+		for i in range(0,int(radios)):
+			logging.info(f"--- Tuner {str(i+1)} @ {str(devices[i])}")
+
+	return hosts, devices
+
+scan()
